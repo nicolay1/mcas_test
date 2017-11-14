0e4475853777bd29296ca2d1fe9fab10b6c97ccb
Fley Nicolas <nicolas.fley@student.ecp.fr>
2017-11-14 17:59:22 +0100

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var options = {
 
 app.use('/', express.static('app', options));
 
-var server = app.listen(8081, function () {
+var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
